0e2677a57da600f2c97dd01f913304d111038994
qwerty12335 <bethiyke90@gmail.com>
2025-10-17 13:05:38 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,10 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'NEXORA BOT',
   author: '‎',
   botName: "NEXORA TECH",
   botOwner: 'FAVOUR AUSTIN', // Your name
-  ownerNumber: '+2347078387622', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '+2347078387622',
+  ownnerNumber:'+23409016748127',//Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
